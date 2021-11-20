0aa3f7094579daf21ff9713ec0073cf89ac798fa
Yurik-s <juriks@mail.ru>
2021-11-20 22:43:00 +0700

    Накидал стартовый (нерабочий) код по работе с клиентом базы Access

--- AutoRepairShop/View/MainWindow.xaml.cs
+++ AutoRepairShop/View/MainWindow.xaml.cs
@@ -20,9 +20,23 @@ namespace AutoRepairShop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};"; // User Id=admin;Password=; - это если база будет защищена паролем
+        // TODO: возможно эта строка подключения более правильная: 
+        // new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=baza1.accdb");
+
+        private string dbSourceFromConfig = "C:\\Users\\Wcoat\\source\\repos\\frozenframe\\auto-repair-shop\\CarRepair.accdb";
+
         public MainWindow()
         {
             InitializeComponent();
+            DbManager dbManager = new DbManager(String.Format(connectionString, dbSourceFromConfig));
+            Client client = new Client("Иванов", "Иван", "Иванович", "+71231234455", "Тестовый");
+            dbManager.addClient(client);
+            List<Client> clients = dbManager.getAllClients();
+
+            foreach(client client in clients) {
+                Console.WriteLine("Фамилия клиента: {0}, Имя: {1}", client.Lastname, client.Name);
+            }
         }
     }
 }
